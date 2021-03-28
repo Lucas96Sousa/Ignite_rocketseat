9b1c653de9e74207f7e4cb39cd0c788e1165bdf7
Lucas Sousa <lucas996oliveira@gmail.com>
2021-03-28 15:23:48 -0300

    create ListCategories functions

--- NODEJS/Aulas/RentalX/src/modules/cars/use-cases/createCategory/CreateCategoryController.ts
+++ NODEJS/Aulas/RentalX/src/modules/cars/use-cases/createCategory/CreateCategoryController.ts
@@ -2,13 +2,10 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 import { Request, Response } from "express";
 
 class CreateCategoryController {
-
-	constructor(private createCategoryUseCase: CreateCategoryUseCase) { }
+	constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
 	handle(req: Request, res: Response) {
 		const { name, description } = req.body;
-
-
 
 		this.createCategoryUseCase.execute({ name, description });
 		return res.status(201).send();
--- NODEJS/Aulas/RentalX/src/modules/cars/use-cases/listCategories/ListCategoriesController.ts
+++ NODEJS/Aulas/RentalX/src/modules/cars/use-cases/listCategories/ListCategoriesController.ts
@@ -1,4 +1,14 @@
-class ListCategoriesController { }
+import { Request, Response } from "express";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
+class ListCategoriesController {
+	constructor(private listCategoryUseCase: ListCategoriesUseCase) {}
 
-export { ListCategoriesController }
+	handle(req: Request, res: Response): Response {
+		const all = this.listCategoryUseCase.execute();
+
+		return res.json(all);
+	}
+}
+
+export { ListCategoriesController };
