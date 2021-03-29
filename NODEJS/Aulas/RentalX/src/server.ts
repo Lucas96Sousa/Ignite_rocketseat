4b4703ea49292968d2892d1f72ac548e990e335a
Lucas Sousa <lucas996oliveira@gmail.com>
2021-03-29 17:50:11 -0300

    Modulo de upload e swagger finalizado

--- NODEJS/Aulas/RentalX/src/server.ts
+++ NODEJS/Aulas/RentalX/src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import routes from "./routes/routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
 app.listen(port, () =>
