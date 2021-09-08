a39b8bf814281f52046f2ac8b9043f3a819358d4
Lucas Sousa <lucas996oliveira@gmail.com>
2021-09-07 21:16:07 -0300

    create card

--- REACT_NATIVE/gofinances/src/components/HighlightCard/styles.ts
+++ REACT_NATIVE/gofinances/src/components/HighlightCard/styles.ts
@@ -1,6 +1,17 @@
 import styled from 'styled-components/native'
 import {Feather} from '@expo/vector-icons'
-export const Container = styled.View``;
+import {RFValue} from 'react-native-responsive-fontsize'
+
+export const Container = styled.View`
+  background-color: ${({theme}) => theme.colors.shape};
+
+  width: ${RFValue(300)}px;
+
+  border-radius: 5px;
+
+  padding: 19px 23px;
+  padding-bottom:${RFValue(42)}px;
+`;
 
 export const Header = styled.View``;
 
