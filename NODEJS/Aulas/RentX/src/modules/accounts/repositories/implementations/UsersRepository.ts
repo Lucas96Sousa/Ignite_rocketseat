3f890c476b2323537d9f4b8f1e73dd015b17a433
Lucas Sousa <lucas996oliveira@gmail.com>
2021-12-14 09:51:49 -0300

    Create UsersRepository Implementation

--- NODEJS/Aulas/RentX/src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ NODEJS/Aulas/RentX/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -1,10 +1,22 @@
+import { getRepository, Repository } from "typeorm";
 import { ICreateUserDTO } from "../../dto/ICreateUserDTO";
+import { User } from "../../entities/User";
 import { IUsersRepository } from "../IUsersRepositories";
 
 
 class UserRepostory implements IUsersRepository {
-	create(data: ICreateUserDTO): Promise<void> {
-		throw new Error("Method not implemented.");
+  private respository: Repository<User>
+
+  constructor () {
+	  this.respository = getRepository(User)
+  }
+
+	async create({name, username, email, driver_license, password}: ICreateUserDTO): Promise<void> {
+		const user = this.respository.create({
+			name, username, email, driver_license, password
+		})
+
+		await this.respository.save(user);
 	}
 }
 
